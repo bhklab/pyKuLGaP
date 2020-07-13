01ea747ca461c081ee5c5d1bcb595adbe1744848
ChristopherEeles <christopher.eeles@outlook.com>
2020-07-13 12:01:40 -0400

    Update: Filled in setup.py with basic package information

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='KuLGaP',
+      version='0.0.1',
+      description="Functions for statistical analysis of treatment response curves in patient derived xenograph (PDX) models of cancer.",
+      url='https://github.com/bhklab/pyKuLGaP/tree/pypi',
+      author='Janosch Ortmann, Christopher Eeles, Benjamin Haibe-Kains',
+      author_email='janosch.ortmann@gmail.com, christopher.eeles@uhnresearch.ca, benjamin.haibe.kains@utoronto.ca',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+      )
