ab5f1ea940241dfd9f77cebc249f915d3a5ab3d3
ChristopherEeles <christopher.eeles@outlook.com>
2020-07-29 18:18:54 -0400

    Fix: Wrong long_description format in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fh:
+    readme = fh.read()
+
 setup(name='pykulgap',
-      version='0.0.3',
+      version='0.0.4',
       description="Functions for statistical analysis of treatment response curves in patient derived xenograph (PDX) models of cancer.",
-      url='https://github.com/bhklab/pyKuLGaP/tree/pypi',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/bhklab/pyKuLGaP/',
       author='Janosch Ortmann, Christopher Eeles, Benjamin Haibe-Kains',
       author_email='janosch.ortmann@gmail.com, christopher.eeles@uhnresearch.ca, benjamin.haibe.kains@utoronto.ca',
       license='MIT',
